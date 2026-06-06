9b31d194012defff3b56395f4846cc5e899388f8
Daniel <dannyfor99@gmail.com>
2026-06-05 22:46:54 -0600

    handle collision en el metodo set

--- DSA/hash_map.ts
+++ DSA/hash_map.ts
@@ -16,17 +16,54 @@ class HashTable {
     }
 
 
-    set(key: any, value: any){
+    set(key: string, value: any){
         const index = this.hash(key);
-        this.table[index] = value;
+        // this.table[index] = value; 
+        let bucket =  this.table[index];
+        if(!bucket){
+            bucket = [[key, value]];
+        } else {
+            const sameKeyItem = bucket.find((item: {key: string}) => item.key === key);
+            if(sameKeyItem){
+                sameKeyItem[1] = value;
+            }else{
+                bucket.push([key, value]);
+            }
+        }
     }
+
 
     get(key: string){
         const index = this.hash(key);
-        
+        // return this.table[index];
+        //TODO: handle collision
+
+    }
+
+    remove(key: string){
+        const index = this.hash(key);
+        this.table[index] = undefined;
+    }
+
+    display(){
+        for(let i = 0; i < this.table.length; i++){
+            if(this.table[i]){
+                console.log(i, this.table[i])
+            }
+
+        }
     }
 
 }
 
 
 const table = new HashTable(50);
+
+table.set("name", "Bruce")
+table.set("age", 25);
+table.display();
+
+console.log(table.get("name"));
+
+// table.remove("name")
+// table.display();
